b9ee70e0bd60e1e6800bb47cd0bf75786fc48815
Wellington Luiz Gaboardi <wgaboardi@gmail.com>
2022-07-13 21:06:57 -0300

    Files after updating

---
+++ pages/posts/novo.js
@@ -0,0 +1,16 @@
+import Alert from '../../components/alert';
+import Layout from '../../components/layout';
+export default function Novo() {
+  return (
+    <Layout>
+      <Alert type='success'>
+        <div>
+          <div>
+            <h1>legal</h1>
+          </div>
+        </div>
+      </Alert>
+    </Layout>
+  )
+
+}
